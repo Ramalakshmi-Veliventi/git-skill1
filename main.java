b0a2c199280135e1d0f2c5e9efd2d116482ce710
Ramalakshmi-Veliventi <ramalakshmiveliventi@gmail.com>
2026-03-18 09:50:14 +0530

    Initial commit

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
-// Bug fix applied on bug-fix branch
+import java.util.Scanner;
 
-//Feature update added
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
